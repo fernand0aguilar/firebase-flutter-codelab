a3d9562ab268ae173bc40b69d4523a0766f53aa0
Fernando Aguilar <fernando@aguilar.net.br>
2019-01-07 10:45:24 -0300

    Create Firebase Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_flutter/myHomePage.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -19,72 +20,4 @@ class MyApp extends StatelessWidget {
       home: MyHomePage(),
     );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() {
-    return _MyHomePageState();
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Startup Name Votes')),
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    // TODO: get actual snapshot from cloud Firestone
-    return _buildList(context, dummySnapshot);
-  }
-
-  Widget _buildList(BuildContext context, List<Map> snapshot){
-    return ListView(
-      padding: const EdgeInsets.only(top: 20.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, Map data){
-    final record = Record.fromMap(data);
-
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.grey),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => print(record),
-        ),
-      ),
-    );
-  }
-}
-
-
-class Record {
-  final String name;  
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-    : assert(map['name'] != null),
-      assert(map['votes'] != null),
-      name = map['name'],
-      votes = map['votes']; 
-
-  Record.fromSnapshot(DocumentSnapshot snapshot) 
-    : this.fromMap(snapshot.data, reference: snapshot.reference);
-  
-  @override
-  String toString() => "Record<$name:$votes>";
 }
---
+++ lib/myHomePage.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_flutter/main.dart';
+import 'package:firebase_flutter/record.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Votes')),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    // TODO: get actual snapshot from cloud Firestone
+    return _buildList(context, dummySnapshot);
+  }
+
+  Widget _buildList(BuildContext context, List<Map> snapshot){
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context, Map data){
+    final record = Record.fromMap(data);
+
+    return Padding(
+      key: ValueKey(record.name),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: ListTile(
+          title: Text(record.name),
+          trailing: Text(record.votes.toString()),
+          onTap: () => print(record),
+        ),
+      ),
+    );
+  }
+}
